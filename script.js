5e3f391a49a402674f35540d399d86c92e729a30
Adilxa <adilhanimerov82@gmail.com>
2025-03-13 19:39:36 +0600

    feat: initial prject

---
+++ script.js
@@ -0,0 +1,73 @@
+// axios
+//   .get("https://jsonplaceholder.typicode.com/posts")
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(error => {
+//     console.error("Error fetching data:", error);
+//   });
+
+// axios.interceptors.request.use(config => {
+//     // Добавление токена аутентификации к каждому запросу
+//     config.headers.Authorization = 'Bearer YOUR_TOKEN';
+//     return config;
+//   }, error => {
+//     return Promise.reject(error);
+//   });
+
+// axios
+//   .post("https://jsonplaceholder.typicode.com/posts", {
+//     title: "foo",
+//     body: "bar",
+//     userId: 1,
+//   })
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(error => {
+//     console.error("Error posting data:", error);
+//   });
+
+// const CancelToken = axios.CancelToken;
+// const source = CancelToken.source();
+
+// axios
+//   .get("https://jsonplaceholder.typicode.com/posts", {
+//     cancelToken: source.token,
+//   })
+//   .catch(thrown => {
+//     if (axios.isCancel(thrown)) {
+//       console.log("Request canceled", thrown.message);
+//     } else {
+//       console.error("Error:", thrown);
+//     }
+//   });
+
+// // Отмена запроса
+// source.cancel("Operation canceled by the user.");
+
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+  timeout: 1000,
+  headers: { "X-Custom-Header": "foobar" },
+});
+
+axiosInstance
+  .get("/posts")
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
+
+axios
+  .get("https://jsonplaceholder.typicode.com/users")
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error("Error fetching data:", error);
+  });
